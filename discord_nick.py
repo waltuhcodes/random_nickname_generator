8b87b293b079d1beffe832a68dece465b437ead9
waltuhcodes <115601787+waltuhcodes@users.noreply.github.com>
2022-10-12 09:18:20 +0700

    Create discord_nick.py

---
+++ discord_nick.py
@@ -0,0 +1,10 @@
+from random import randrange
+
+idk = ["walter", "waltuh", "floppa", "bingus"]
+
+def main():
+    print(idk[randrange(0, 4)])
+
+main()
+
+# prints a random username i'll use for the day
